d00c55e2d27743ee1b2725102d8c1d3adf57a382
Saenius <66512810+Saenius@users.noreply.github.com>
2020-06-28 15:57:14 +0300

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,68 @@
+const numDivs = 36;
+const maxHits = 10;
+
+let hits = 0;
+let missHits = 0;
+let firstHitTime = 0;
+
+function round() {
+
+
+  $('.game-field').removeClass("target");
+  $('.game-field').html("");
+
+  let divSelector = randomDivId();
+  $(divSelector).addClass("target");
+
+  $(divSelector).html(hits + 1);
+
+  if (hits === 0) {
+    firstHitTime = getTimestamp();
+  }
+
+
+  // FIXME: тут надо определять при первом клике firstHitTime
+
+  if (hits === maxHits) {
+    endGame();
+  }
+}
+
+function endGame() {
+  
+  $(".game-field").hide();
+  let totalPlayedMillis = getTimestamp() - firstHitTime;
+  let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
+  $("#total-time-played").text(totalPlayedSeconds);
+  $("#total-misshits").text(hits - missHits);
+  $("#win-message").removeClass("d-none");
+}
+
+function handleClick(event) {
+  
+  if ($(event.target).hasClass("target")) {
+    hits = hits + 1;
+    round();
+  } else {
+    missHits = missHits + 1;
+    $(event.target).addClass("miss");
+  }
+  // TODO: как-то отмечать если мы промахнулись? См CSS класс .miss
+}
+
+function init() {
+  
+  
+  $(".game-field").click(handleClick);
+
+  $("#button-start").click(function() {
+    round();
+    $("#button-start").hide();
+  });
+
+  $("#button-reload").click(function() {
+    location.reload();
+  });
+}
+
+$(document).ready(init);
